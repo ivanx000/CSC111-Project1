ca59089718a8d67a5c5f33cf82bc634cb9dd3fde
ivanx000 <ivanxie102@gmail.com>
2025-01-23 19:05:10 -0500

    Event dataclass

--- ex1/ex1_event_logger.py
+++ ex1/ex1_event_logger.py
@@ -49,7 +49,11 @@ class Event:
     # set of attributes, you can do that separately in the project1 folder. This class is part of
     # Exercise 1 and will be auto-graded.
 
-    # TODO: Add attributes below based on the provided descriptions above. Use the specified datatypes.
+    id_num: int
+    description: str
+    next_command: Optional[str]
+    next: Optional[Event]
+    prev: Optional[Event]
 
 
 class EventList:
